fd8ec1b2c4141028207b12b0ec6d64092da09c29
AdamKorcz <adam@adalogics.com>
2021-12-30 15:36:03 +0000

    etcd: small build fix

--- projects/etcd/build.sh
+++ projects/etcd/build.sh
@@ -17,7 +17,7 @@ sed -i '88 a return' $SRC/etcd/client/pkg/testutil/testutil.go
 compile_go_fuzzer . FuzzKVProxy fuzz_kv_proxy
 # Remove fuzzer to avoid duplicate global variables:
 rm grpc_proxy_fuzzer.go
-if [ "$SANITIZER" != "coverage" ]
+if [ "$SANITIZER" = "coverage" ]
 then
 	rm fuzzkvproxy_test.go
 fi	
