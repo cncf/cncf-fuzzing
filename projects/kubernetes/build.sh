3223fb8ed3a969081471e97c673529920890fa9f
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-02-10 12:07:29 +0000

    kubernetes: Rewrite parser fuzzers to native go fuzzers (#92)

--- projects/kubernetes/build.sh
+++ projects/kubernetes/build.sh
@@ -1,10 +1,30 @@
+#!/bin/bash -eu
+# Copyright 2022 ADA Logics Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 set -o nounset
 set -o pipefail
 set -o errexit
 set -x
 
+
+#export PATH=$PATH:/usr/local/go/bin
 # Autogenerate the marshaling fuzzer
 cd $SRC/kubernetes
+go get github.com/AdaLogics/go-fuzz-headers@latest
+
 mkdir $SRC/kubernetes/test/fuzz/fuzzing
 grep -r ") Marshal()" . > $SRC/grep_result.txt
 mv $SRC/cncf-fuzzing/projects/kubernetes/autogenerate.py ./
@@ -36,19 +56,25 @@ mv $SRC/kubernetes/pkg/kubelet/kubelet_test.go \
 mv $SRC/kubernetes/pkg/kubelet/kubelet_node_status_test.go \
    $SRC/kubernetes/pkg/kubelet/kubelet_node_status_test_fuzz.go
 
-cp $SRC/cncf-fuzzing/projects/kubernetes/*.go \
-   $SRC/kubernetes/test/fuzz/fuzzing/
+mv $SRC/cncf-fuzzing/projects/kubernetes/mount-utils_fuzzer.go \
+   $SRC/kubernetes/staging/src/k8s.io/mount-utils/
 
 mv $SRC/cncf-fuzzing/projects/kubernetes/deployment_util_fuzzer.go \
    $SRC/kubernetes/pkg/controller/deployment/util/
 
+rm $SRC/cncf-fuzzing/projects/kubernetes/parser_fuzzer.go
+cp $SRC/cncf-fuzzing/projects/kubernetes/*.go \
+   $SRC/kubernetes/test/fuzz/fuzzing/
 
-go mod tidy && go mod vendor
+
 # disable this fuzzer for now
 #compile_go_fuzzer k8s.io/kubernetes/pkg/kubelet/server FuzzRequest fuzz_request
 
-# Add the swagger.json content to the kubectl fuzzer
 cd $SRC/kubernetes/test/fuzz/fuzzing
+go mod tidy && go mod vendor
+
+
+# Add the swagger.json content to the kubectl fuzzer
 apt-get update && apt-get install -y wget
 wget https://raw.githubusercontent.com/kubernetes/kubernetes/master/staging/src/k8s.io/kubectl/testdata/openapi/swagger.json
 sed -i 's/`//g' swagger.json
@@ -56,6 +82,31 @@ echo -e "\nvar swaggerjson = \`">>kubectl_fuzzer.go
 cat swagger.json>>kubectl_fuzzer.go
 echo -e "\`">>kubectl_fuzzer.go
 compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzCreateElement fuzz_create_element
+
+mkdir native_fuzzing && cd native_fuzzing
+sed 's/go 1.16/go 1.18/g' -i $SRC/kubernetes/go.mod
+printf "package main\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/utils\"\n" > register.go
+
+gotip get github.com/AdamKorcz/go-118-fuzz-build/utils
+gotip mod tidy
+gotip mod vendor
+
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseQuantity fuzz_parse_quantity
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzMeta1ParseToLabelSelector fuzz_meta1_parse_to_label_selector
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseSelector fuzz_parse_selector
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzLabelsParse fuzz_labels_parse
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseGroupVersion fuzz_parse_group_version
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseResourceArg fuzz_parse_resource_arg
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseVersion fuzz_parse_version
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParsePrivateKeyPEM fuzz_parse_private_pem
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParsePublicKeysPEM fuzz_parse_public_keys_pem
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseHostPort fuzz_parse_host_port
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzUrlsMatch fuzz_urls_match
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseCSR fuzz_parse_csr
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseEnv fuzz_parse_env
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseQOSReserve fuzz_parse_qos_reserve
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseCPUSet fuzz_parse_cpu_set
+compile_native_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseImageName fuzz_parse_image_name
 
 compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzApiMarshaling fuzz_api_marshaling
 compile_go_fuzzer k8s.io/kubernetes/pkg/kubelet/kuberuntime FuzzKubeRuntime fuzz_kube_runtime
@@ -83,20 +134,5 @@ compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzReadLogs fuzz_read_log
 compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzRoundtrip fuzz_roundtrip
 compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzSetDefaults_KubeSchedulerConfiguration fuzz_set_defaults_kube_scheduler_configuration
 compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzAllValidation fuzz_all_validation
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseQuantity fuzz_parse_quantity
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzMeta1ParseToLabelSelector fuzz_meta1_parse_to_label_selector
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseSelector fuzz_parse_selector
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzLabelsParse fuzz_labels_parse
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseGroupVersion fuzz_parse_group_version
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseResourceArg fuzz_parse_resource_arg
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseVersion fuzz_parse_version
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParsePrivateKeyPEM fuzz_parse_private_pem
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParsePublicKeysPEM fuzz_parse_public_keys_pem
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseHostPort fuzz_parse_host_port
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzUrlsMatch fuzz_urls_match
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseCSR fuzz_parse_csr
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseEnv fuzz_parse_env
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseQOSReserve fuzz_parse_qos_reserve
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseCPUSet fuzz_parse_cpu_set
-compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzParseImageName fuzz_parse_image_name
 
+cd $SRC/kubernetes
---
+++ projects/kubernetes/native_go_parser_fuzzers_test.go
@@ -0,0 +1,219 @@
+// Copyright 2022 ADA Logics Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package fuzzing
+
+import (
+	"io"
+	"testing"
+
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/version"
+	"k8s.io/client-go/util/keyutil"
+	envutil "k8s.io/kubectl/pkg/cmd/set/env"
+	"k8s.io/kubectl/pkg/util/certificate"
+	kubeadmutil "k8s.io/kubernetes/cmd/kubeadm/app/util"
+	"k8s.io/kubernetes/pkg/credentialprovider"
+	"k8s.io/kubernetes/pkg/kubelet/cm"
+	"k8s.io/kubernetes/pkg/kubelet/cm/cpuset"
+	"k8s.io/kubernetes/pkg/util/parsers"
+)
+
+// FuzzParseQuantity implements a fuzzer
+// that targets resource.ParseQuantity
+func FuzzParseQuantity(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _ = resource.ParseQuantity(data)
+	})
+}
+
+// FuzzMeta1ParseToLabelSelector implements a fuzzer
+// that targets metav1.ParseToLabelSelector
+func FuzzMeta1ParseToLabelSelector(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _ = metav1.ParseToLabelSelector(data)
+	})
+}
+
+// FuzzParseSelector implements a fuzzer
+// that targets fields.ParseSelector
+func FuzzParseSelector(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _ = fields.ParseSelector(data)
+	})
+}
+
+// FuzzLabelsParse implements a fuzzer
+// that targets labels.Parse
+func FuzzLabelsParse(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _ = labels.Parse(data)
+	})
+}
+
+// FuzzParseGroupVersion implements a fuzzer
+// that targets schema.ParseGroupVersion
+func FuzzParseGroupVersion(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _ = schema.ParseGroupVersion(data)
+	})
+}
+
+// FuzzParseResourceArg implements a fuzzer
+// that targets schema.ParseResourceArg
+func FuzzParseResourceArg(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _ = schema.ParseResourceArg(string(data))
+	})
+}
+
+// FuzzParseVersion implements a fuzzer
+// that targets:
+// - version.ParseSemantic,
+// - version/(*Version).String()
+// - version.ParseGeneric
+// - version/(*Version).AtLeast(*Version)
+func FuzzParseVersion(f *testing.F) {
+	f.Fuzz(func(t *testing.T, vString1, vString2 string) {
+
+		v1, err := version.ParseSemantic(vString1)
+		if err != nil {
+			return
+		}
+
+		// Test if the Version will crash (*Version).String()
+		_ = v1.String()
+
+		v2, err := version.ParseGeneric(vString2)
+		if err != nil {
+			return
+		}
+		_ = v1.AtLeast(v2)
+	})
+}
+
+// FuzzParsePrivateKeyPEM implements a fuzzer
+// that targets keyutil.ParsePrivateKeyPEM
+func FuzzParsePrivateKeyPEM(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = keyutil.ParsePrivateKeyPEM(data)
+	})
+}
+
+// FuzzParsePublicKeysPEM implements a fuzzer
+// that targets keyutil.ParsePublicKeysPEM
+func FuzzParsePublicKeysPEM(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = keyutil.ParsePublicKeysPEM(data)
+	})
+}
+
+// FuzzParseHostPort implements a fuzzer
+// that targets kubeadmutil.ParseHostPort
+func FuzzParseHostPort(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _, _ = kubeadmutil.ParseHostPort(data)
+	})
+}
+
+// FuzzUrlsMatch implements a fuzzer
+// that targets credentialprovider.URLsMatchStr
+func FuzzUrlsMatch(f *testing.F) {
+	f.Fuzz(func(t *testing.T, glob, target string) {
+		_, _ = credentialprovider.URLsMatchStr(glob, target)
+	})
+}
+
+// FuzzParseCSR implements a fuzzer
+// that targets certificate.ParseCSR
+func FuzzParseCSR(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		_, _ = certificate.ParseCSR(data)
+	})
+}
+
+// FuzzParseEnv implements a fuzzer
+// that targets envutil.ParseEnv
+func FuzzParseEnv(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		f := fuzz.NewConsumer(data)
+
+		// Create a pseudo-random spec.
+		// Will be used as argument to the fuzz target
+
+		// length of slice:
+		qty, err := f.GetInt()
+		if err != nil {
+			return
+		}
+		spec := make([]string, qty, qty)
+
+		// fill slice with values
+		for i := 0; i < qty; i++ {
+			s, err := f.GetString()
+			if err != nil {
+				return
+			}
+			spec = append(spec, s)
+		}
+		var r io.Reader
+		_, _, _, _ = envutil.ParseEnv(spec, r)
+	})
+}
+
+// FuzzParseQOSReserve implements a fuzzer
+// that targets cm.ParseQOSReserved
+func FuzzParseQOSReserve(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		f := fuzz.NewConsumer(data)
+
+		// Create a pseudo-random map.
+		// Will be used as argument to the fuzz target
+		m := make(map[string]string)
+		err := f.FuzzMap(&m)
+		if err != nil {
+			return
+		}
+		_, _ = cm.ParseQOSReserved(m)
+	})
+}
+
+// FuzzParseCPUSet implements a fuzzer
+// that targets:
+// - cpuset.Parse
+// - cpuset/(CPUSet).String
+func FuzzParseCPUSet(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		cs, err := cpuset.Parse(data)
+		if err != nil {
+			return
+		}
+		_ = cs.String()
+	})
+}
+
+// FuzzParseImageName implements a fuzzer
+// that targets parsers.ParseImageName
+func FuzzParseImageName(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data string) {
+		_, _, _, _ = parsers.ParseImageName(data)
+	})
+}
