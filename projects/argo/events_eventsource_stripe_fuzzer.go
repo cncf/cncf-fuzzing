de9f53ec88f4737b65ba1f0b41e98d25e7a9e050
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-04-26 13:10:00 +0100

    events stripe fuzzer: Activate logSAN (#157)

--- projects/argo/events_eventsource_stripe_fuzzer.go
+++ projects/argo/events_eventsource_stripe_fuzzer.go
@@ -2,29 +2,76 @@ package stripe
 
 import (
 	"bytes"
-	"fmt"
-	"net/http"
 	"io"
-	"github.com/argoproj/argo-events/eventsources/common/webhook"
-	"github.com/argoproj/argo-events/pkg/apis/eventsource/v1alpha1"
-)
+	"net/http"
+	"os"
 
+	"github.com/argoproj/argo-events/eventsources/common/webhook"
+
+	zap "go.uber.org/zap"
+
+	"github.com/argoproj/argo-events/pkg/apis/eventsource/v1alpha1"
+
+	"github.com/AdaLogics/go-fuzz-headers/sanitizers/logsanitizer"
+)
 
 var (
-	router = &Router{
-		route:             webhook.GetFakeRoute(),
-		stripeEventSource: &v1alpha1.StripeEventSource{},
-	}
+	logger     = NewFuzzLogger()
+	fRoute     = webhook.GetFakeRoute()
+	router     *Router
+	logFileAbs = "/tmp/argo-logfile"
 )
 
+func init() {
+
+	fRoute.Logger = logger
+	router = &Router{
+		route:             fRoute,
+		stripeEventSource: &v1alpha1.StripeEventSource{},
+	}
+}
+
+func NewFuzzLogger() *zap.SugaredLogger {
+	var config zap.Config
+	config = zap.NewProductionConfig()
+	// Config customization goes here if any
+	config.OutputPaths = []string{logFileAbs}
+	logger, err := config.Build()
+	if err != nil {
+		panic(err)
+	}
+	return logger.Named("argo-events").Sugar()
+}
+
+func setupLogSanitizer() (*logsanitizer.Sanitizer, *os.File, error) {
+	logFile, err := os.Create(logFileAbs)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	logSanitizer := logsanitizer.NewSanitizer()
+	logSanitizer.SetLogFile(logFileAbs)
+	return logSanitizer, logFile, nil
+}
+
 func FuzzStripeEventsource(data []byte) int {
+	logSAN, logFp, err := setupLogSanitizer()
+	if err != nil {
+		panic(err)
+	}
+
+	defer runLogSanitizer(logSAN, logFp)
+
 	router.route.Active = true
 	writer := &webhook.FakeHttpWriter{}
-	fmt.Println("body: ")
-	fmt.Println(string(data))
-	fmt.Println("------------------")
 	router.HandleRoute(writer, &http.Request{
 		Body: io.NopCloser(bytes.NewReader(data)),
 	})
 	return 1
+}
+
+func runLogSanitizer(logSAN *logsanitizer.Sanitizer, logFp *os.File) {
+	logSAN.CheckLogfile()
+	logFp.Close()
+	os.Remove(logFileAbs)
 }
