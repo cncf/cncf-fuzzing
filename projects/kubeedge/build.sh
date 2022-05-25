9c2bf3f4a9e4d22e87a16854b5e9838c902b3594
AdamKorcz <adam@adalogics.com>
2022-05-25 14:57:47 +0100

    kubeedge: add fuzzers
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/kubeedge/build.sh
+++ projects/kubeedge/build.sh
@@ -3,7 +3,21 @@ set -o pipefail
 set -o errexit
 set -x
 
-cp $SRC/cncf-fuzzing/lane_fuzzer.go $SRC/kubeedge/staging/src/github.com/kubeedge/viaduct/pkg/lane/
+cp $SRC/cncf-fuzzing/projects/kubeedge/lane_fuzzer.go $SRC/kubeedge/staging/src/github.com/kubeedge/viaduct/pkg/lane/
 cd $SRC/kubeedge/staging/src/github.com/kubeedge/viaduct
 sed '94d' -i $SRC/kubeedge/staging/src/github.com/kubeedge/viaduct/pkg/translator/message.go
 compile_go_fuzzer github.com/kubeedge/viaduct/pkg/lane FuzzLaneReadMessage fuzz_lane_read_message
+
+cp $SRC/cncf-fuzzing/projects/kubeedge/dtmanager_fuzzer.go $SRC/kubeedge/edge/pkg/devicetwin/dtmanager/
+mv $SRC/kubeedge/edge/pkg/devicetwin/dtmanager/twin_test.go $SRC/kubeedge/edge/pkg/devicetwin/dtmanager/twint_test_fuzz.go
+cd $SRC/kubeedge
+go mod tidy && go mod vendor
+
+# remove logs
+sed 's/klog\./\/\/klog\./g' -i $SRC/kubeedge/edge/pkg/devicetwin/dtmanager/twin.go
+sed 's/\"k8s\.io\/klog\/v2"/\/\/\"k8s\.io\/klog\/v2\"/g' -i $SRC/kubeedge/edge/pkg/devicetwin/dtmanager/twin.go
+
+sed 's/klog\./\/\/klog\./g' -i $SRC/kubeedge/edge/pkg/devicetwin/dtcontext/dtcontext.go
+sed 's/\"k8s\.io\/klog\/v2"/\/\/\"k8s\.io\/klog\/v2\"/g' -i $SRC/kubeedge/edge/pkg/devicetwin/dtcontext/dtcontext.go
+compile_go_fuzzer github.com/kubeedge/kubeedge/edge/pkg/devicetwin/dtmanager FuzzdealTwinUpdate fuzz_deal_twin_update
+compile_go_fuzzer github.com/kubeedge/kubeedge/edge/pkg/devicetwin/dtmanager FuzzdealTwinGet fuzz_deal_twin_get
---
+++ projects/kubeedge/dtmanager_fuzzer.go
@@ -0,0 +1,66 @@
+// Copyright 2022 ADA Logics Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package dtmanager
+
+import (
+
+	"github.com/kubeedge/beehive/pkg/core/model"
+	fuzz "github.com/AdaLogics/go-fuzz-headers"
+)
+
+func FuzzdealTwinUpdate(data []byte) int {
+	f := fuzz.NewConsumer(data)
+	deviceA, err := f.GetString()
+	if err != nil {
+		return 0
+	}
+	deviceB, err := f.GetString()
+	if err != nil {
+		return 0
+	}
+	content, err := f.GetBytes()
+	if err != nil {
+		return 0
+	}
+	msg := &model.Message{
+		Content: content,
+	}
+	context := contextFunc(deviceA)
+	dealTwinUpdate(&context, deviceB, msg)
+	return 1
+}
+
+func FuzzdealTwinGet(data []byte) int {
+	f := fuzz.NewConsumer(data)
+	deviceA, err := f.GetString()
+	if err != nil {
+		return 0
+	}
+	deviceB, err := f.GetString()
+	if err != nil {
+		return 0
+	}
+	content, err := f.GetBytes()
+	if err != nil {
+		return 0
+	}
+	msg := &model.Message{
+		Content: content,
+	}
+	context := contextFunc(deviceA)
+	dealTwinGet(&context, deviceB, msg)
+	return 1
+}
