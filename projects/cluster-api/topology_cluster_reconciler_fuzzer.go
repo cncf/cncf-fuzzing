709b541c5cc5ddfe2860771621b53b086d5c5b18
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-06-13 12:41:49 +0100

    cluster-api: Fix broken build (#188)
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/cluster-api/topology_cluster_reconciler_fuzzer.go
+++ projects/cluster-api/topology_cluster_reconciler_fuzzer.go
@@ -21,16 +21,11 @@ import (
 
 	"github.com/ghodss/yaml"
 	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/client-go/tools/record"
 	//ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
-	"sigs.k8s.io/cluster-api/internal/contract"
-	"sigs.k8s.io/cluster-api/internal/controllers/topology/cluster/mergepatch"
 	"sigs.k8s.io/cluster-api/internal/controllers/topology/cluster/scope"
 	"sigs.k8s.io/cluster-api/internal/test/builder"
 	//"sigs.k8s.io/cluster-api/internal/test/envtest"
@@ -61,74 +56,6 @@ func initFunc() {
 	_ = corev1.AddToScheme(fakeSchemeForFuzzing)
 }
 
-// tests cluster-api/internal/controllers/topology/cluster.(r *Reconciler).reconcileMachineHealthCheck()
-func FuzzreconcileMachineHealthCheck(data []byte) int {
-	initter.Do(initFunc)
-	f := fuzz.NewConsumer(data)
-
-	current := &clusterv1.MachineHealthCheck{}
-	desired := &clusterv1.MachineHealthCheck{}
-
-	err := f.GenerateStruct(current)
-	if err != nil {
-		return 0
-	}
-	err = f.GenerateStruct(desired)
-	if err != nil {
-		return 0
-	}
-	cp := builder.ControlPlane(metav1.NamespaceDefault, "cp1").Build()
-	cp.SetUID("very-unique-identifier")
-
-	r := Reconciler{
-		Client: fake.NewClientBuilder().
-			WithScheme(fakeSchemeForFuzzing).
-			WithObjects([]client.Object{cp}...).
-			Build(),
-		recorder: record.NewFakeRecorder(32),
-	}
-	// Fuzz target:
-	r.reconcileMachineHealthCheck(fuzzCtx, current, desired)
-	return 1
-}
-
-// tests cluster-api/internal/controllers/topology/cluster.(r *Reconciler).reconcileReferencedObject()
-func FuzzreconcileReferencedObject(data []byte) int {
-	f := fuzz.NewConsumer(data)
-	cluster := &clusterv1.Cluster{}
-	err := f.GenerateStruct(cluster)
-	if err != nil {
-		return 0
-	}
-	current, err := GetUnstructured(f)
-	if err != nil {
-		return 0
-	}
-	desired, err := GetUnstructured(f)
-	if err != nil {
-		return 0
-	}
-
-	fakeClient := fake.NewClientBuilder().WithScheme(fakeSchemeForFuzzing).Build()
-	r := Reconciler{
-		Client: fakeClient,
-		// NOTE: Intentionally using a fake recorder, so the test can also be run without testenv.
-		recorder: record.NewFakeRecorder(32),
-	}
-
-	r.reconcileReferencedObject(fuzzCtx, reconcileReferencedObjectInput{
-		cluster: cluster,
-		current: current,
-		desired: desired,
-		opts: []mergepatch.HelperOption{
-			mergepatch.AuthoritativePaths{
-				// Note: Just using .spec.machineTemplate.metadata here as an example.
-				contract.ControlPlane().MachineTemplate().Metadata().Path(),
-			},
-		}})
-	return 1
-}
-
 // helper function to crate an unstructured object.
 func GetUnstructured(f *fuzz.ConsumeFuzzer) (*unstructured.Unstructured, error) {
 	yamlStr, err := f.GetString()
@@ -151,56 +78,6 @@ func validateBlueprint(b *scope.ClusterBlueprint) error {
 		return fmt.Errorf("ClusterClass.Spec.ControlPlane.MachineInfrastructure is nil")
 	}
 	return nil
-}
-
-// tests cluster-api/internal/controllers/topology/cluster.(r *Reconciler).reconcileInfrastructureCluster()
-func FuzzreconcileControlPlane(data []byte) int {
-	f := fuzz.NewConsumer(data)
-	cluster := &clusterv1.Cluster{}
-	err := f.GenerateStruct(cluster)
-	if err != nil {
-		return 0
-	}
-
-	desiredCluster := &clusterv1.Cluster{}
-	err = f.GenerateStruct(desiredCluster)
-	if err != nil {
-		return 0
-	}
-	bp := &scope.ClusterBlueprint{}
-	err = f.GenerateStruct(bp)
-	if err != nil {
-		return 0
-	}
-	err = validateBlueprint(bp)
-	if err != nil {
-		return 0
-	}
-	desired := &scope.ClusterState{
-		Cluster: desiredCluster,
-		ControlPlane: &scope.ControlPlaneState{
-			Object: builder.ControlPlane("ns1", "controlplane1").
-				WithVersion("v1.21.2").
-				WithReplicas(3).
-				Build(),
-		},
-	}
-	s := scope.New(cluster)
-	s.Desired = desired
-	s.Blueprint = bp
-
-	fakeClient := fake.NewClientBuilder().WithScheme(fakeSchemeForFuzzing).Build()
-	r := Reconciler{
-		Client: fakeClient,
-		// NOTE: Intentionally using a fake recorder, so the test can also be run without testenv.
-		recorder: record.NewFakeRecorder(32),
-	}
-	fmt.Printf("%+v\n", s)
-	err = r.reconcileControlPlane(fuzzCtx, s)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return 1
 }
 
 func validateUnstructured(unstr *unstructured.Unstructured) error {
