6aa97fc13c41f7422019b958f1800f99122396ee
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-07-13 23:07:55 +0100

    cilium: Add payload fuzzer (#202)
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/cilium/build.sh
+++ projects/cilium/build.sh
@@ -1,12 +1,16 @@
-cp $SRC/cncf-fuzzing/projects/cilium/elf_fuzzer.go $SRC/cilium/pkg/elf/
-cp $SRC/cncf-fuzzing/projects/cilium/bpf_fuzzer.go $SRC/cilium/pkg/bpf/
-cp $SRC/cncf-fuzzing/projects/cilium/matchpattern_fuzzer.go $SRC/cilium/pkg/fqdn/matchpattern/
-cp $SRC/cncf-fuzzing/projects/cilium/hubble_parser_fuzzer.go $SRC/cilium/pkg/hubble/parser/
-cp $SRC/cncf-fuzzing/projects/cilium/labels_fuzzer.go $SRC/cilium/pkg/k8s/slim/k8s/apis/labels/
+export CILIUM=$SRC/cncf-fuzzing/projects/cilium
+
+cp $CILIUM/elf_fuzzer.go $SRC/cilium/pkg/elf/
+cp $CILIUM/bpf_fuzzer.go $SRC/cilium/pkg/bpf/
+cp $CILIUM/matchpattern_fuzzer.go $SRC/cilium/pkg/fqdn/matchpattern/
+cp $CILIUM/hubble_parser_fuzzer.go $SRC/cilium/pkg/hubble/parser/
+cp $CILIUM/labels_fuzzer.go $SRC/cilium/pkg/k8s/slim/k8s/apis/labels/
 mv $SRC/cilium/proxylib/cassandra/cassandraparser_test.go $SRC/cilium/proxylib/cassandra/cp_test_fuzz.go
-mv $SRC/cncf-fuzzing/projects/cilium/OnData_fuzzer.go $SRC/cilium/proxylib/cassandra/
+mv $CILIUM/OnData_fuzzer.go $SRC/cilium/proxylib/cassandra/
+mv $CILIUM/payload_fuzzer.go $SRC/cilium/pkg/monitor/payload
 go mod tidy && go mod vendor
 
+compile_go_fuzzer github.com/cilium/cilium/pkg/monitor/payload FuzzPayloadEncodeDecode FuzzPayloadEncodeDecode
 compile_go_fuzzer github.com/cilium/cilium/pkg/elf FuzzElfOpen fuzz_elf_open
 compile_go_fuzzer github.com/cilium/cilium/pkg/bpf FuzzBpf fuzz_bpf
 compile_go_fuzzer github.com/cilium/cilium/pkg/fqdn/matchpattern FuzzMatchpatternValidate fuzz_matchpattern_validate
