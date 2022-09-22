3ff31f7c4157c357b302ecbfe37aa78261b53164
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2022-09-22 20:34:09 +0100

    cilium: fix broken build (#228)
    Signed-off-by: AdamKorcz <adam@adalogics.com>
    
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/cilium/build.sh
+++ projects/cilium/build.sh
@@ -1,7 +1,17 @@
+# install Go 1.19
+apt-get update && apt-get install -y wget
+cd $SRC
+wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
+
+mkdir temp-go
+rm -rf /root/.go/*
+tar -C temp-go/ -xzf go1.19.linux-amd64.tar.gz
+mv temp-go/go/* /root/.go/
+cd $SRC/cilium
+
 export CILIUM=$SRC/cncf-fuzzing/projects/cilium
 
 cp $CILIUM/elf_fuzzer.go $SRC/cilium/pkg/elf/
-cp $CILIUM/bpf_fuzzer.go $SRC/cilium/pkg/bpf/
 cp $CILIUM/matchpattern_fuzzer.go $SRC/cilium/pkg/fqdn/matchpattern/
 cp $CILIUM/hubble_parser_fuzzer.go $SRC/cilium/pkg/hubble/parser/
 cp $CILIUM/labels_fuzzer.go $SRC/cilium/pkg/k8s/slim/k8s/apis/labels/
@@ -23,7 +33,6 @@ compile_go_fuzzer github.com/cilium/cilium/pkg/monitor/format FuzzFormatEvent fu
 compile_go_fuzzer github.com/cilium/cilium/pkg/monitor/payload FuzzPayloadEncodeDecode FuzzPayloadEncodeDecode
 compile_go_fuzzer github.com/cilium/cilium/pkg/elf FuzzElfOpen fuzz_elf_open
 compile_go_fuzzer github.com/cilium/cilium/pkg/elf FuzzElfWrite fuzz_elf_write
-compile_go_fuzzer github.com/cilium/cilium/pkg/bpf FuzzBpf fuzz_bpf
 compile_go_fuzzer github.com/cilium/cilium/pkg/fqdn/matchpattern FuzzMatchpatternValidate fuzz_matchpattern_validate
 compile_go_fuzzer github.com/cilium/cilium/pkg/fqdn/matchpattern FuzzMatchpatternValidateWithoutCache fuzz_matchpattern_validate_without_cache
 compile_go_fuzzer github.com/cilium/cilium/pkg/hubble/parser FuzzParserDecode fuzz_parser_decode
