6783c1bc04dd1bee1c8198d752359c879d359a32
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2023-01-02 14:04:35 +0000

    knative: fix small nit (#257)
    Signed-off-by: AdamKorcz <adam@adalogics.com>
    
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/knative/build.sh
+++ projects/knative/build.sh
@@ -23,7 +23,7 @@ cp $CNCFFuzzing/json_fuzzer.go $SRC/pkg/webhook/json/
 mv $SRC/pkg/webhook/json/decode_test.go $SRC/pkg/webhook/json/decode_test_fuzz.go
 compile_go_fuzzer knative.dev/pkg/webhook/json FuzzJsonDecode fuzz_json_decode
 
-cp $SRC/fuzz_configmaps.go $SRC/pkg/webhook/configmaps/
+cp $CNCFFuzzing/fuzz_configmaps.go $SRC/pkg/webhook/configmaps/
 mv $SRC/pkg/webhook/configmaps/configmaps_test.go $SRC/pkg/webhook/configmaps/configmaps_fuzz.go
 compile_native_go_fuzzer knative.dev/pkg/webhook/configmaps FuzzAdmit FuzzAdmit
 
