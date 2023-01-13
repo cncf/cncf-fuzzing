42d9d086119da759f8051322bd7227a2b9dcfff6
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2023-01-13 23:03:22 +0000

    notary: add fuzzer (#275)
    Signed-off-by: AdamKorcz <adam@adalogics.com>
    
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/notary/build.sh
+++ projects/notary/build.sh
@@ -3,6 +3,7 @@ sed 's/go 1.17/go 1.19/g' -i $SRC/notary/go.mod
 export CNCFFuzz="${SRC}/cncf-fuzzing/projects/notary"
 
 cp $CNCFFuzz/fuzz_trustmanager_test.go $SRC/notary/trustmanager/
+cp $CNCFFuzz/fuzz_tuf_utils.go $SRC/notary/tuf/utils/
 
 printf "package trustmanager\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/notary/trustmanager/registerfuzzdep.go
 go mod edit -replace github.com/AdaLogics/go-fuzz-headers=github.com/AdamKorcz/go-fuzz-headers-1@1f10f66a31bf0e5cc26a2f4a74bd3be5f6463b67
@@ -13,6 +14,7 @@ mv $SRC/notary/trustmanager/keys_test.go $SRC/notary/trustmanager/keys_test_fuzz
 mv $SRC/notary/trustmanager/keystore_test.go $SRC/notary/trustmanager/keystore_test_fuzz.go
 compile_native_go_fuzzer github.com/theupdateframework/notary/trustmanager FuzzImportKeysSimple FuzzImportKeysSimple
 compile_native_go_fuzzer github.com/theupdateframework/notary/trustmanager FuzzImportKeysStructured FuzzImportKeysStructured
+compile_native_go_fuzzer github.com/theupdateframework/notary/tuf/utils FuzzParsePEMPrivateKey FuzzParsePEMPrivateKey
 
 mv $SRC/cncf-fuzzing/projects/notary/fuzz_handlers.go $SRC/notary/server/handlers/
 compile_native_go_fuzzer github.com/theupdateframework/notary/server/handlers FuzzAtomicUpdateHandler FuzzAtomicUpdateHandler
