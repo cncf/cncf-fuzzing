dffb81bdb3a365b83cca569e9ff48c031fd8f872
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2023-01-20 16:56:45 +0000

    knative: add more targets to validation fuzzer (#305)
    Signed-off-by: AdamKorcz <adam@adalogics.com>
    
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/knative/fuzz_validation.go
+++ projects/knative/fuzz_validation.go
@@ -21,6 +21,12 @@ import (
 
 	fuzz "github.com/AdaLogics/go-fuzz-headers"
 
+	eventingv1 "knative.dev/eventing/pkg/apis/eventing/v1"
+	eventingv1beta1 "knative.dev/eventing/pkg/apis/eventing/v1beta1"
+	flowsv1 "knative.dev/eventing/pkg/apis/flows/v1"
+	messagingv1 "knative.dev/eventing/pkg/apis/messaging/v1"
+	sourcesv1 "knative.dev/eventing/pkg/apis/sources/v1"
+	sourcesv1beta2 "knative.dev/eventing/pkg/apis/sources/v1beta2"
 	"knative.dev/pkg/apis"
 	autoscalingv1alpha1 "knative.dev/serving/pkg/apis/autoscaling/v1alpha1"
 	servingv1alpha1 "knative.dev/serving/pkg/apis/serving/v1alpha1"
@@ -29,14 +35,27 @@ import (
 
 var (
 	types = map[int]string{
-		0: "Revision",
-		1: "PodAutoscaler",
-		2: "Metric",
-		3: "Configuration",
-		4: "Route",
-		5: "Service",
-		6: "v1alpha1_DomainMapping",
-		7: "v1beta1_DomainMapping",
+		0:  "Revision",
+		1:  "PodAutoscaler",
+		2:  "Metric",
+		3:  "Configuration",
+		4:  "Route",
+		5:  "Service",
+		6:  "v1alpha1_DomainMapping",
+		7:  "v1beta1_DomainMapping",
+		8:  "flows_v1_Parallel",
+		9:  "flows_v1_Sequence",
+		10: "sources_v1beta2_PingSource",
+		11: "sourcesv1_SinkBinding",
+		12: "sourcesv1_PingSource",
+		13: "sourcesv1_ApiServerSource",
+		14: "sourcesv1_ContainerSource",
+		15: "eventingv1beta1_EventType",
+		16: "eventingv1_Broker",
+		17: "eventingv1_Trigger",
+		18: "messagingv1_Channel",
+		19: "messagingv1_InMemoryChannel",
+		20: "messagingv1_Subscription",
 	}
 )
 
@@ -116,6 +135,122 @@ func FuzzValidation(f *testing.F) {
 			ctx := apis.WithinSubResourceUpdate(ctxBackground, dm2, sr)
 			dm1.Validate(ctx)
 
+		case "flows_v1_Parallel":
+			p1 := &flowsv1.Parallel{}
+			ff.GenerateStruct(p1)
+			p2 := &flowsv1.Parallel{}
+			ff.GenerateStruct(p2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, p2, sr)
+			p1.Validate(ctx)
+
+		case "flows_v1_Sequence":
+			s1 := &flowsv1.Sequence{}
+			ff.GenerateStruct(s1)
+			s2 := &flowsv1.Sequence{}
+			ff.GenerateStruct(s2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, s2, sr)
+			s1.Validate(ctx)
+
+		case "sources_v1beta2_PingSource":
+			p1s := &sourcesv1beta2.PingSource{}
+			ff.GenerateStruct(p1s)
+			ps2 := &sourcesv1beta2.PingSource{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "sourcesv1_SinkBinding":
+			p1s := &sourcesv1.SinkBinding{}
+			ff.GenerateStruct(p1s)
+			ps2 := &sourcesv1.SinkBinding{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "sourcesv1_PingSource":
+			p1s := &sourcesv1.PingSource{}
+			ff.GenerateStruct(p1s)
+			ps2 := &sourcesv1.PingSource{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "sourcesv1_ApiServerSource":
+			p1s := &sourcesv1.ApiServerSource{}
+			ff.GenerateStruct(p1s)
+			ps2 := &sourcesv1.ApiServerSource{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "sourcesv1_ContainerSource":
+			p1s := &sourcesv1.ContainerSource{}
+			ff.GenerateStruct(p1s)
+			ps2 := &sourcesv1.ContainerSource{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "eventingv1beta1_EventType":
+			p1s := &eventingv1beta1.EventType{}
+			ff.GenerateStruct(p1s)
+			ps2 := &eventingv1beta1.EventType{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "eventingv1_Broker":
+			p1s := &eventingv1.Broker{}
+			ff.GenerateStruct(p1s)
+			ps2 := &eventingv1.Broker{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "eventingv1_Trigger":
+			p1s := &eventingv1.Trigger{}
+			ff.GenerateStruct(p1s)
+			ps2 := &eventingv1.Trigger{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "messagingv1_Channel":
+			p1s := &messagingv1.Channel{}
+			ff.GenerateStruct(p1s)
+			ps2 := &messagingv1.Channel{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "messagingv1_InMemoryChannel":
+			p1s := &messagingv1.InMemoryChannel{}
+			ff.GenerateStruct(p1s)
+			ps2 := &messagingv1.InMemoryChannel{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
+
+		case "messagingv1_Subscription":
+			p1s := &messagingv1.InMemoryChannel{}
+			ff.GenerateStruct(p1s)
+			ps2 := &messagingv1.InMemoryChannel{}
+			ff.GenerateStruct(ps2)
+			ctxBackground := context.Background()
+			ctx := apis.WithinSubResourceUpdate(ctxBackground, ps2, sr)
+			p1s.Validate(ctx)
 		}
 
 	})
