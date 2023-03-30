a2c66e8db1a88d0953e50066c103cc3185bd7963
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2023-03-30 22:05:15 +0100

    knative: print stack trace in fuzzer in case of a panic (#337)
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/knative/fuzz_activatornet.go
+++ projects/knative/fuzz_activatornet.go
@@ -2,7 +2,9 @@ package net
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"runtime/debug"
 	"testing"
 
 	fuzz "github.com/AdaLogics/go-fuzz-headers"
@@ -34,6 +36,14 @@ func NewFuzzLogger() *zap.SugaredLogger {
 
 func FuzzNewRevisionThrottler(f *testing.F) {
 	f.Fuzz(func(t *testing.T, data []byte) {
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("fatal error: out of memory")
+				debug.PrintStack()
+				panic("fatal error: out of memory")
+
+			}
+		}()
 		ff := fuzz.NewConsumer(data)
 
 		ip1, err := ff.GetString()
