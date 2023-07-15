ae1718d1f2f7246bf6ce1750c97a917ab82ba1ed
AdamKorcz <adam@adalogics.com>
2023-07-15 18:42:03 +0100

    kyverno: initial commit
    Signed-off-by: AdamKorcz <adam@adalogics.com>

---
+++ projects/kyverno/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -eu
+# Copyright 2023 the cncf-fuzzing authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+export CNCFFuzzing="${SRC}/cncf-fuzzing/projects/kyverno"
+
+cp $CNCFFuzzing/fuzz_policy_test.go $SRC/kyverno/pkg/validation/policy/
+
+printf "package engine\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/kyverno/pkg/engine/registerfuzzdep.go
+go mod edit -replace github.com/AdaLogics/go-fuzz-headers=github.com/AdamKorcz/go-fuzz-headers-1@1f10f66a31bf0e5cc26a2f4a74bd3be5f6463b67
+go mod tidy
+
+cp $CNCFFuzzing/fuzz_engine_test.go $SRC/kyverno/pkg/engine/
+compile_native_go_fuzzer github.com/kyverno/kyverno/pkg/engine FuzzVerifyImageAndPatchTest FuzzVerifyImageAndPatchTest
+compile_native_go_fuzzer github.com/kyverno/kyverno/pkg/engine FuzzEngineValidateTest FuzzEngineValidateTest
+compile_native_go_fuzzer github.com/kyverno/kyverno/pkg/engine FuzzMutateTest FuzzMutateTest
+compile_native_go_fuzzer github.com/kyverno/kyverno/pkg/validation/policy FuzzValidatePolicy FuzzValidatePolicy
