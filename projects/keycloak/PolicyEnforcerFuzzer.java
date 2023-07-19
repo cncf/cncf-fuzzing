3735a0718e45816a53f79050067577789f1aea19
Arthur Chan <arthur.chan@adalogics.com>
2023-07-19 12:13:46 +0000

    Fix fuzzer logic and comments
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/keycloak/PolicyEnforcerFuzzer.java
+++ projects/keycloak/PolicyEnforcerFuzzer.java
@@ -16,6 +16,7 @@
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.EnumSet;
 import java.util.Map;
@@ -33,23 +34,28 @@ import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
 import org.mockito.Mockito;
 
 /**
- * This fuzzer targets the methods in the PolicyEnforcer
- * class of the authz package.
+ * This fuzzer creates configuration objects and mock
+ * HttpRequest and HttpResponse to fuzz the enforce
+ * method of the PolicyEnforcer class of the authz package.
  */
 public class PolicyEnforcerFuzzer {
   public static void fuzzerTestOneInput(FuzzedDataProvider data) {
     try {
-      // Initialize enforcer config instance
+      // Initialize enforcer config instance with random set of request and response data
       PolicyEnforcerConfig enforcerConfig = new PolicyEnforcerConfig();
+      // Randomly choose the enforcement mode
       enforcerConfig.setEnforcementMode(
           data.pickValue(EnumSet.allOf(PolicyEnforcerConfig.EnforcementMode.class)));
-      enforcerConfig.setOnDenyRedirectTo(data.consumeString(data.remainingBytes() / 2));
+      // Randomly set the redirect string
+      enforcerConfig.setOnDenyRedirectTo("");
+      // Randomly turn on and off for using http method as scope
       enforcerConfig.setHttpMethodAsScope(data.consumeBoolean());
-      enforcerConfig.setRealm(data.consumeString(data.remainingBytes() / 2));
-      enforcerConfig.setAuthServerUrl(data.consumeString(data.remainingBytes() / 2));
-      enforcerConfig.setResource(data.consumeString(data.remainingBytes() / 2));
+      // Randomly config the string data for the config
+      enforcerConfig.setAuthServerUrl("");
+      enforcerConfig.setRealm("");
+      enforcerConfig.setResource("");
 
-      // Prepare credential map
+      // Prepare credential map with random data
       Map<String, Object> map = enforcerConfig.getCredentials();
       map.put(data.consumeString(data.remainingBytes() / 2),
           data.consumeString(data.remainingBytes() / 2));
@@ -59,6 +65,8 @@ public class PolicyEnforcerFuzzer {
       ClientCredentialsProvider provider =
           ClientCredentialsProviderUtils.bootstrapClientAuthenticator(new AdapterConfig());
 
+      // Build the policy enforcer with random data and the config and provider object initialised
+      // above
       PolicyEnforcer enforcer = PolicyEnforcer.builder()
                                     .clientId(data.consumeString(data.remainingBytes() / 2))
                                     .bearerOnly(data.consumeBoolean())
@@ -71,18 +79,21 @@ public class PolicyEnforcerFuzzer {
       HttpServletResponse servletResponse = Mockito.mock(HttpServletResponse.class);
       TokenPrincipal principal = Mockito.mock(TokenPrincipal.class);
 
+      // Mock key metod of the HttpServletRequest, HttpServletResponse and TokenPrincipal instance
+      // Use the mock method to deny real HTTP request and simulate the response with random data
       Mockito.when(servletRequest.getParameter(data.consumeString(data.remainingBytes() / 2)))
           .thenReturn(data.consumeString(data.remainingBytes() / 2));
       Mockito.when(servletResponse.getWriter())
-          .thenReturn(new PrintWriter(data.consumeString(data.remainingBytes() / 2)));
+          .thenReturn(new PrintWriter(data.consumeRemainingAsString()));
       Mockito.when(principal.getToken()).thenReturn(new AccessToken());
 
-      // Prepare ServletHttpRequest and ServletHttpResponse
+      // Prepare HttpRequest and HttpResponse instance with the mocked object
       HttpRequest request = new ServletHttpRequest(servletRequest, principal);
       HttpResponse response = new ServletHttpResponse(servletResponse);
 
+      // Fuzz the enforce method with the mocked object and random data
       enforcer.enforce(request, response);
-    } catch (Exception e) {
+    } catch (IOException | RuntimeException e) {
       // Known exception thrown directly from method above.
     }
   }
