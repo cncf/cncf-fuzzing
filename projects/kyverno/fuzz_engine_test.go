9ddabf0c73ff23ec79c22c629df0f3dc55312afa
AdamKorcz <44787359+AdamKorcz@users.noreply.github.com>
2023-07-19 23:56:21 +0100

    kyverno: optimize validation fuzzer (#428)
    Signed-off-by: AdamKorcz <adam@adalogics.com>

--- projects/kyverno/fuzz_engine_test.go
+++ projects/kyverno/fuzz_engine_test.go
@@ -19,6 +19,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"strings"
 	"testing"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -35,6 +37,12 @@ import (
 	"github.com/kyverno/kyverno/pkg/engine/policycontext"
 	"github.com/kyverno/kyverno/pkg/registryclient"
 	kubeutils "github.com/kyverno/kyverno/pkg/utils/kube"
+	"github.com/kyverno/kyverno/pkg/autogen"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+
+	fuzz "github.com/AdamKorcz/go-fuzz-headers-1"
 )
 
 /*
@@ -142,41 +150,299 @@ func newFuzzPolicyContext(
 	return p, nil
 }
 
-func testValidateFuzz(
-	ctx context.Context,
-	rclient registryclient.Client,
-	pContext *PolicyContext,
-	cfg config.Configuration,
-	contextLoader engineapi.ContextLoaderFactory,
-) engineapi.EngineResponse {
-	if contextLoader == nil {
-		contextLoader = factories.DefaultContextLoaderFactory(nil)
+func createPolicySpec(ff *fuzz.ConsumeFuzzer) (kyverno.Spec, error) {
+	spec := &kyverno.Spec{}
+	rules := createRules(ff)
+	if len(rules) == 0 {
+		return *spec, fmt.Errorf("no rules")
+	} 
+	spec.Rules = rules
+
+	applyAll, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
 	}
-	e := NewEngine(
-		cfg,
+	if applyAll {
+		aa := kyverno.ApplyAll
+		spec.ApplyRules = &aa
+	} else {
+		ao := kyverno.ApplyOne
+		spec.ApplyRules = &ao
+	}
+
+	failPolicy, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	if failPolicy {
+		fa := kyverno.Fail
+		spec.FailurePolicy = &fa
+	} else {
+		ig := kyverno.Ignore
+		spec.FailurePolicy = &ig
+	}
+
+	setValidationFailureAction, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	if setValidationFailureAction {
+		audit, err := ff.GetBool()
+		if err != nil {
+			return *spec, err
+		}
+		if audit {
+			spec.ValidationFailureAction = "Audit"
+		} else {
+			spec.ValidationFailureAction = "Enforce"
+		}
+	}
+
+	setValidationFailureActionOverrides, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	if setValidationFailureActionOverrides {
+		vfao := make([]kyverno.ValidationFailureActionOverride, 0)
+		ff.CreateSlice(&vfao)
+		if len(vfao) != 0 {
+			spec.ValidationFailureActionOverrides = vfao
+		}
+	}
+
+	admission, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	spec.Admission = &admission
+
+	background, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	spec.Background = &background
+
+	schemaValidation, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	spec.SchemaValidation = &schemaValidation
+
+	mutateExistingOnPolicyUpdate, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	spec.MutateExistingOnPolicyUpdate = mutateExistingOnPolicyUpdate 
+
+	generateExistingOnPolicyUpdate, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	spec.GenerateExistingOnPolicyUpdate = &generateExistingOnPolicyUpdate 
+
+	generateExisting, err := ff.GetBool()
+	if err != nil {
+		return *spec, err
+	}
+	spec.GenerateExisting = generateExisting
+
+	return *spec, nil
+}
+
+func createRules(ff *fuzz.ConsumeFuzzer) []kyverno.Rule {
+	rules := make([]kyverno.Rule, 0)
+	noOfRules, err := ff.GetInt()
+	if err != nil {
+		return rules
+	}
+	for i:=0; i<noOfRules%100; i++ {
+		rule, err := createRule(ff)
+		if err != nil {
+			return rules
+		}
+
+		rules = append(rules, *rule)
+	}
+	return rules
+}
+
+func createRule(f *fuzz.ConsumeFuzzer) (*kyverno.Rule, error) {
+	rule := &kyverno.Rule{}
+	name, err := f.GetString()
+	if err != nil {
+		return rule, err
+	}
+	rule.Name = name
+
+	setContext, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setContext {
+		c := make([]kyverno.ContextEntry, 0)
+		f.CreateSlice(&c)
+		if len(c) != 0 {
+			rule.Context = c
+		}
+	}
+
+	mr := &kyverno.MatchResources{}
+	f.GenerateStruct(mr)
+	rule.MatchResources = *mr
+
+	setExcludeResources, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setExcludeResources {
+		er := &kyverno.MatchResources{}
+		f.GenerateStruct(mr)
+		rule.ExcludeResources = *er
+	}
+
+	setRawAnyAllConditions, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setRawAnyAllConditions {
+		raac := &apiextv1.JSON{}
+		f.GenerateStruct(raac)
+		rule.RawAnyAllConditions  = raac
+	}
+
+	setCELPreconditions, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setCELPreconditions {
+		celp := make([]admissionregistrationv1.MatchCondition, 0)
+		f.CreateSlice(&celp)
+		if len(celp) != 0 {
+			rule.CELPreconditions = celp
+		}
+	}
+
+	setMutation, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setMutation {
+		m := &kyverno.Mutation{}
+		f.GenerateStruct(m)
+		rule.Mutation = *m
+	}
+
+	setValidation, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setValidation {
+		v := &kyverno.Validation{}
+		f.GenerateStruct(v)
+		rule.Validation = *v
+	}
+
+	setGeneration, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setGeneration {
+		g := &kyverno.Generation{}
+		f.GenerateStruct(g)
+		rule.Generation = *g
+	}
+
+	setVerifyImages, err := f.GetBool()
+	if err != nil {
+		return rule, err
+	}
+	if setVerifyImages {
+		iv := make([]kyverno.ImageVerification, 0)
+		f.CreateSlice(&iv)
+		if len(iv) != 0 {
+			rule.VerifyImages = iv
+		}
+	}
+
+	return rule, nil
+}
+
+func createUnstructuredObject(f *fuzz.ConsumeFuzzer) string {
+	var sb strings.Builder
+
+	sb.WriteString("{ \"apiVersion\": \"apps/v1\", \"kind\": \"Deployment\", \"metadata\": { \"creationTimestamp\": \"2020-09-21T12:56:35Z\", \"name\": \"fuzz\", \"labels\": { \"test\": \"qos\" } }, \"spec\": { ")
+	
+	for i:=0;i<1000;i++ {
+		typeToAdd, err := f.GetInt()
+		if err != nil {
+			return sb.String()
+		}
+		switch typeToAdd%11 {
+		case 0:
+			sb.WriteString("\"")
+		case 1:
+			s, err := f.GetString()
+			if err != nil {
+				return sb.String()
+			}
+			sb.WriteString(s)
+		case 2:
+			sb.WriteString("{")
+		case 3:
+			sb.WriteString("}")
+		case 4:
+			sb.WriteString("[")
+		case 5:
+			sb.WriteString("]")
+		case 6:
+			sb.WriteString(":")
+		case 7:
+			sb.WriteString(",")
+		case 8:
+			sb.WriteString(" ")
+		case 9:
+			sb.WriteString("\t")
+		case 10:
+			sb.WriteString("\n")
+		}
+	}
+	return sb.String()
+}
+
+var regClient = registryclient.NewOrDie()
+var validateEngine = NewEngine(
+		fuzzCfg,
 		config.NewDefaultMetricsConfiguration(),
 		fuzzJp,
 		nil,
-		factories.DefaultRegistryClientFactory(adapters.RegistryClient(rclient), nil),
-		contextLoader,
+		factories.DefaultRegistryClientFactory(adapters.RegistryClient(regClient), nil),
+		factories.DefaultContextLoaderFactory(nil),
 		nil,
 		"",
 	)
-	return e.Validate(
-		ctx,
-		pContext,
-	)
-}
+var validateContext = context.Background()
 
 func FuzzEngineValidateTest(f *testing.F) {
-	f.Fuzz(func(t *testing.T, rawResource, rawPolicy []byte) {
-		var policy kyverno.ClusterPolicy
-		err := json.Unmarshal(rawPolicy, &policy)
+	f.Fuzz(func(t *testing.T, data []byte) {
+		ff := fuzz.NewConsumer(data)
+		//ff.GenerateStruct(policy)
+		cpSpec, err := createPolicySpec(ff)
 		if err != nil {
 			return
 		}
+		policy := &kyverno.ClusterPolicy{}
+		policy.Spec = cpSpec
 
-		resourceUnstructured, err := kubeutils.BytesToUnstructured(rawResource)
+		if len(autogen.ComputeRules(policy)) == 0 {
+			return
+		}
+
+		rawResource := createUnstructuredObject(ff)
+		if rawResource == "" {
+			return
+		}
+		
+		resourceUnstructured, err := kubeutils.BytesToUnstructured([]byte(rawResource))
 		if err != nil {
 			return
 		}
@@ -185,8 +451,10 @@ func FuzzEngineValidateTest(f *testing.F) {
 		if err != nil {
 			return
 		}
-		testValidateFuzz(context.Background(), registryclient.NewOrDie(), pc.WithPolicy(&policy), fuzzCfg, nil)
-
+		validateEngine.Validate(
+			validateContext,
+			pc.WithPolicy(policy),
+		)
 	})
 }
 
