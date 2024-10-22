29cfe1857a885211ce009cbab6cfbc7036328e69
Arthur Chan <arthur.chan@adalogics.com>
2024-10-22 11:16:57 +0000

    Keycloak: Fix unexpected NPE
    Signed-off-by: Arthur Chan <arthur.chan@adalogics.com>

--- projects/keycloak/TokenVerifierFuzzer.java
+++ projects/keycloak/TokenVerifierFuzzer.java
@@ -21,7 +21,12 @@ import org.keycloak.representations.JsonWebToken;
 public class TokenVerifierFuzzer {
   public static void fuzzerTestOneInput(FuzzedDataProvider data) {
     try {
-      TokenVerifier.create(data.consumeRemainingAsString(), JsonWebToken.class).verify();
+      TokenVerifier verifier = TokenVerifier.create(data.consumeRemainingAsString(), JsonWebToken.class);
+      if ((verifier.getHeader() == null) || (verifier.getHeader().getAlgorithm() == null)) {
+        // Malformed JsonWebToken, skipping to next iteration.
+        return;
+      }
+      verifier.verify();
     } catch (VerificationException e) {
       // Known exception
     }
