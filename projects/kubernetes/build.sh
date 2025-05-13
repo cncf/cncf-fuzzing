6ba1c0c8cf00fe1d2cb0a861b7045761299696b1
thezero <io@thezero.org>
2025-05-13 16:58:34 +0200

    add Kubernetes-CEL expression fuzzer
    Signed-off-by: thezero <io@thezero.org>

--- projects/kubernetes/build.sh
+++ projects/kubernetes/build.sh
@@ -175,3 +175,5 @@ fi
 if [ "$SANITIZER" != "coverage" ]; then
    compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzApiMarshaling fuzz_api_marshaling
 fi
+
+compile_go_fuzzer k8s.io/kubernetes/test/fuzz/fuzzing FuzzCelCompile fuzz_compile
