237e5dc7075a6c6052a1c39a85985a612ece0b5f
Joe Stringer <joe@isovalent.com>
2025-08-20 23:43:41 -0700

    cilium: Remove latest upstreamed fuzzers
    These fuzzers are now pushed into github.com/cilium/cilium, so there is
    no need to duplicate them here. Remove them from the tree.
    
    Signed-off-by: Joe Stringer <joe@isovalent.com>

--- projects/cilium/build.sh
+++ projects/cilium/build.sh
@@ -3,19 +3,6 @@ cd $SRC/cilium
 
 export CILIUM=$SRC/cncf-fuzzing/projects/cilium
 
-cp $CILIUM/matchpattern_fuzzer.go $SRC/cilium/pkg/fqdn/matchpattern/
-cp $CILIUM/hubble_parser_fuzzer.go $SRC/cilium/pkg/hubble/parser/
-cp $CILIUM/labels_fuzzer.go $SRC/cilium/pkg/k8s/slim/k8s/apis/labels/
-mv $CILIUM/labelsfilter_fuzzer.go $SRC/cilium/pkg/labelsfilter/
-printf "package v2\nimport _ \"github.com/AdamKorcz/go-118-fuzz-build/testing\"\n" > $SRC/cilium/pkg/k8s/apis/cilium.io/v2/registerfuzzdep.go
-go mod tidy && go mod vendor
-
-compile_go_fuzzer github.com/cilium/cilium/pkg/labelsfilter FuzzLabelsfilterPkg fuzz_labelsfilter_pkg
-compile_go_fuzzer github.com/cilium/cilium/pkg/fqdn/matchpattern FuzzMatchpatternValidate fuzz_matchpattern_validate
-compile_go_fuzzer github.com/cilium/cilium/pkg/fqdn/matchpattern FuzzMatchpatternValidateWithoutCache fuzz_matchpattern_validate_without_cache
-compile_go_fuzzer github.com/cilium/cilium/pkg/hubble/parser FuzzParserDecode fuzz_parser_decode
-compile_go_fuzzer github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/labels FuzzLabelsParse fuzz_labels_parse
-
 cd $SRC/proxy
 mv $CILIUM/OnData_fuzzer.go $SRC/proxy/proxylib/cassandra/
 mv $SRC/proxy/proxylib/cassandra/cassandraparser_test.go $SRC/proxy/proxylib/cassandra/cassandraparser_test_fuzz.go
