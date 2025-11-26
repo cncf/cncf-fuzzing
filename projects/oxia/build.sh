72f6ec302c9776b7e6a227610f313cbc1529ffae
Adam Korczynski <adam@adalogics.com>
2025-11-26 21:46:33 +0000

    oxia: initial integration
    Signed-off-by: Adam Korczynski <adam@adalogics.com>

---
+++ projects/oxia/build.sh
@@ -0,0 +1,74 @@
+#!/bin/bash -eu
+# Copyright 2025 CNCF-Fuzzing authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+
+cd /tmp
+export GOROOT=/root/.go
+wget https://go.dev/dl/go1.25.3.linux-amd64.tar.gz
+
+mkdir temp-go
+tar -C temp-go/ -xzf go1.25.3.linux-amd64.tar.gz
+
+rm -r /root/.go
+mkdir /root/.go/
+mv temp-go/go/* /root/.go/
+rm -rf temp-go
+
+
+cd $SRC/go-118-fuzz-build
+go build
+mv go-118-fuzz-build $GOPATH/bin/go-118-fuzz-build_v2
+pushd cmd/convertLibFuzzerTestcaseToStdLibGo
+  go build . && mv convertLibFuzzerTestcaseToStdLibGo $GOPATH/bin/
+popd
+pushd cmd/addStdLibCorpusToFuzzer
+  go build . && mv addStdLibCorpusToFuzzer $GOPATH/bin/
+popd
+cd $SRC/oxia
+
+
+echo building fuzzers
+mkdir -p $SRC/oxia/oxia/fuzz
+cp $SRC/cncf-fuzzing/projects/oxia/*.go $SRC/oxia/oxia/fuzz/
+
+fuzz_targets1=(
+	FuzzKVPutGet
+	FuzzKVRangeScan
+	FuzzKVDeleteRange
+	FuzzKVComparisonTypes
+	FuzzKVKeyOrdering
+	FuzzMetadataLoadStore
+	FuzzMetadataLeaderHelper
+	FuzzE2EOperations
+	FuzzShardStatusUnmarshalInvalid
+	FuzzServerGetIdentifier
+	FuzzWalAppendRead
+	FuzzWalMultipleEntries
+	FuzzHierarchicalEncodeDecode
+	FuzzNaturalEncodeDecode
+	FuzzEncodingPreservesOrder
+	FuzzHierarchicalSortingProperties
+	FuzzCompareWithSlashProperties
+	FuzzSortingStability
+	FuzzDatabaseRangeOperations
+	FuzzDatabaseKeyValidation
+)
+
+cd $SRC/oxia/oxia/fuzz
+PKG="github.com/oxia-db/oxia/oxia/fuzz"
+for f in "${fuzz_targets1[@]}"; do
+  compile_native_go_fuzzer_v2 "$PKG" "$f" "$f"
+done
---
+++ projects/oxia/fuzz_wal_test.go
@@ -0,0 +1,162 @@
+// Copyright 2025 the cncf-fuzzing authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////
+
+package fuzz
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/oxia-db/oxia/common/constant"
+	"github.com/oxia-db/oxia/common/proto"
+	"github.com/oxia-db/oxia/oxiad/dataserver/wal"
+)
+
+// createTestWal creates a WAL for fuzzing with small segment size
+// to exercise segment transitions frequently.
+func createTestWal(t *testing.T, segmentSize int32) (wal.Factory, wal.Wal) {
+	t.Helper()
+	dir := t.TempDir()
+	f := wal.NewWalFactory(&wal.FactoryOptions{
+		BaseWalDir:  dir,
+		Retention:   1 * time.Hour,
+		SegmentSize: segmentSize,
+		SyncData:    false, // Faster for fuzzing
+	})
+	w, err := f.NewWal(constant.DefaultNamespace, 100, nil)
+	if err != nil {
+		t.Fatalf("failed to create WAL: %v", err)
+	}
+	return f, w
+}
+
+// FuzzWalAppendRead tests basic append/read roundtrip on WAL.
+func FuzzWalAppendRead(f *testing.F) {
+	// Seeds
+	f.Add([]byte("hello"))
+	f.Add([]byte{0, 1, 2, 3, 4})
+	f.Add(bytes.Repeat([]byte{0xff}, 100))
+
+	f.Fuzz(func(t *testing.T, value []byte) {
+		if len(value) == 0 {
+			return
+		}
+
+		factory, w := createTestWal(t, 128*1024)
+		defer factory.Close()
+		defer w.Close()
+
+		// Append
+		entry := &proto.LogEntry{
+			Term:   1,
+			Offset: 0,
+			Value:  value,
+		}
+		err := w.Append(entry)
+		if err != nil {
+			t.Fatalf("append failed: %v", err)
+		}
+
+		// Read back via reader
+		reader, err := w.NewReader(wal.InvalidOffset)
+		if err != nil {
+			t.Fatalf("failed to create reader: %v", err)
+		}
+		defer reader.Close()
+
+		if !reader.HasNext() {
+			t.Fatal("reader should have next")
+		}
+
+		readEntry, err := reader.ReadNext()
+		if err != nil {
+			t.Fatalf("read failed: %v", err)
+		}
+
+		if !bytes.Equal(value, readEntry.Value) {
+			t.Fatalf("value mismatch: wrote %v, read %v", value, readEntry.Value)
+		}
+	})
+}
+
+// FuzzWalMultipleEntries tests appending multiple entries with varying content.
+func FuzzWalMultipleEntries(f *testing.F) {
+	// Seeds: concatenated values with length prefixes
+	f.Add([]byte{3, 'a', 'b', 'c', 2, 'd', 'e'})      // 2 entries
+	f.Add([]byte{1, 'x', 1, 'y', 1, 'z'})             // 3 single-byte entries
+	f.Add(bytes.Repeat([]byte{5, 1, 2, 3, 4, 5}, 10)) // 10 identical entries
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		// Parse data into variable-length entries
+		var values [][]byte
+		i := 0
+		for i < len(data) {
+			length := int(data[i])
+			i++
+			if length == 0 || i+length > len(data) {
+				break
+			}
+			values = append(values, data[i:i+length])
+			i += length
+		}
+
+		if len(values) == 0 {
+			return
+		}
+
+		factory, w := createTestWal(t, 64*1024) // Small segment size
+		defer factory.Close()
+		defer w.Close()
+
+		// Append all entries
+		for idx, value := range values {
+			entry := &proto.LogEntry{
+				Term:   1,
+				Offset: int64(idx),
+				Value:  value,
+			}
+			err := w.Append(entry)
+			if err != nil {
+				t.Fatalf("append %d failed: %v", idx, err)
+			}
+		}
+
+		// Read all back with forward reader
+		reader, err := w.NewReader(wal.InvalidOffset)
+		if err != nil {
+			t.Fatalf("failed to create reader: %v", err)
+		}
+		defer reader.Close()
+
+		for idx, expectedValue := range values {
+			if !reader.HasNext() {
+				t.Fatalf("reader should have entry %d", idx)
+			}
+			readEntry, err := reader.ReadNext()
+			if err != nil {
+				t.Fatalf("read %d failed: %v", idx, err)
+			}
+			if !bytes.Equal(expectedValue, readEntry.Value) {
+				t.Fatalf("entry %d mismatch", idx)
+			}
+		}
+
+		if reader.HasNext() {
+			t.Fatal("reader should be exhausted")
+		}
+	})
+}
